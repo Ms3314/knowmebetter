1173727661c838a45c11a87ab13804bf280ef303
Samiuddin <mdsamiuddin2005@outlook.com>
2024-12-28 22:24:24 +0530

    db connected

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection:ConnectionObject = {}
+
+async function dbConnect():Promise<void> {
+    if(connection.isConnected) {
+        console.log("Already connected to the db");
+        return 
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI || '')
+        connection.isConnected = db.connections[0].readyState ;
+        console.log('DB connected succesfully')
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+
+}
+
+export default dbConnect()
+
+//Assignment 
+// check the db ke connection ku console.log karke thoda study karke dekho 
--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,5 +3,5 @@ import {z} from 'zod'
 export const MessageSchema = z.object({
     // this can be called anything be it a username or be it an email 
     content : z.string().min(10 , {message : 'Contnt must be of atleast 10 charaters'}).max(300 , {message : 'Contnt must be of maximum 3000 charaters'}) , 
-
+    
 })
