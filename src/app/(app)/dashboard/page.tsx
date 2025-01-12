88f587bcabc0d0f5ba8b7b69b4227d9953b162eb
Samiuddin <mdsamiuddin2005@outlook.com>
2025-01-13 00:19:34 +0530

    added the styles over here copied

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -9,6 +9,11 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import axios from 'axios'
 import { userAgent } from 'next/server';
+import MessageCard from '@/components/MessageCard';
+import { Loader2, RefreshCcw } from 'lucide-react';
+import { Button } from '@react-email/components';
+import { Separator } from '@radix-ui/react-separator';
+import { Switch } from '@radix-ui/react-switch';
 
 const Page = () => {
     // iska jo type hai wah message hai as we are adding it init 
@@ -104,9 +109,76 @@ const Page = () => {
         </div>
     }
 
+    const { username } = session.user as User;
+    const baseUrl = `${window.location.protocol}//${window.location.host}`;
+    const profileUrl = `${baseUrl}/u/${username}`;
+    const copyToClipboard = () => {
+        console.log(session.user as User);
+        navigator.clipboard.writeText(profileUrl);
+        toast({
+        title: "URL Copied",
+        description: "Profile URL copied to clipboard",
+        });
+    };
+
     return (
-    <div>
-      
+      <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{" "}
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <Switch
+          {...register("acceptMessages")}
+          checked={acceptMessages}
+          onCheckedChange={handleSwitchChange}
+          disabled={isSwitchLoading}
+        />
+        <span className="ml-2">
+          Accept Messages: {acceptMessages ? "On" : "Off"}
+        </span>
+      </div>
+      <Separator />
+
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {isLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {messages.length > 0 ? (
+          messages.map((message, index) => (
+            <MessageCard
+              key={index} 
+            //@ts-expect-error-the problem is with maybe we dont know the types of the messages
+              message={message}
+              onMessageDelete={handleDeleteMessage}
+            />
+          ))
+        ) : (
+          <p>No messages to display.</p>
+        )}
+      </div>
     </div>
   )
 }
